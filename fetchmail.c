1765f38ca8bffd1b6fa1a2a8fe947388763612f0
Jia Jie Zheng <jiajizheng@student.unimelb.edu.au>
2023-12-13 17:32:25 +1100

    testing more things, BIO_read does not read the full response but I also cannot just do the full reponse because it is blocking and will just get stuck

--- fetchmail.c
+++ fetchmail.c
@@ -6,8 +6,6 @@
 
 #define MAX_BUFFER_SIZE 10240
 
-
-
 void send_receive_ssl(BIO *bio, const char *send_cmd, char *recv_buffer, size_t buffer_size) {
     // Send command
     BIO_puts(bio, send_cmd);
@@ -22,6 +20,7 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
+    // Throwaway Gmail account created for this project, password is the app password
     const char *hostname = argv[1];
     const char *username = "tzeng0088@gmail.com";
     const char *password = "vhkycyvvnoxcoejj";
@@ -56,12 +55,7 @@ int main(int argc, char *argv[]) {
     send_receive_ssl(bio, login_command, login_response, sizeof(login_response));
     printf("Login Response: %s\n", login_response);
 
-
-    // // Check if login was successful
-    // if (strncmp(login_response, "A01 OK", 6) != 0) {
-    //     fprintf(stderr, "Error: Login failed.\n");
-    //     return EXIT_FAILURE;
-    // }
+    
 
     // Select the INBOX folder
     char select_command[] = "A02 SELECT INBOX\r\n";
@@ -69,25 +63,24 @@ int main(int argc, char *argv[]) {
     send_receive_ssl(bio, select_command, select_response, sizeof(select_response));
     printf("Select Response: %s\n", select_response);
 
-    // // Check if folder selection was successful
-    // if (strncmp(select_response, "A02 OK", 6) != 0) {
-    //     fprintf(stderr, "Error: Failed to select the INBOX folder.\n");
-    //     return EXIT_FAILURE;
-    // }
-
     // Fetch the first email
     char fetch_command[] = "A03 FETCH 1 BODY.PEEK[]\r\n";
     char fetch_response[MAX_BUFFER_SIZE];
+    // char combined[102400];
+    // send_receive_ssl(bio, fetch_command, fetch_response, 10240);
+    // for (int i = 0; i < 20; i++) {
+    //     //BIO_read(bio, fetch_response, 10240);
+    //     send_receive_ssl(bio, fetch_command, fetch_response, 10240);
+    //     strcat(combined, fetch_response);
+    // }
+    // printf("combined:\n%s\n", combined);
     send_receive_ssl(bio, fetch_command, fetch_response, sizeof(fetch_response));
-    BIO_read(bio, fetch_response, sizeof(fetch_response));
-    BIO_read(bio, fetch_response, sizeof(fetch_response));
-    BIO_read(bio, fetch_response, sizeof(fetch_response));
-    BIO_read(bio, fetch_response, sizeof(fetch_response));
-    BIO_read(bio, fetch_response, sizeof(fetch_response));
-    BIO_read(bio, fetch_response, sizeof(fetch_response));
-    BIO_read(bio, fetch_response, sizeof(fetch_response));
+
     // Display the email body
     printf("Email Body:\n%s\n", fetch_response);
+    
+    
+    
 
     // Clean up
     BIO_free_all(bio);
