361f43adf3d777c2cecac19e93d3be423f4f62d2
Jia Jie Zheng <jiajizheng@student.unimelb.edu.au>
2023-12-17 17:41:54 +1100

    using select() to determine if data is available or not

--- fetchmail.c
+++ fetchmail.c
@@ -3,10 +3,26 @@
 #include <string.h>
 #include <openssl/ssl.h>
 #include <openssl/bio.h>
-
+#include <sys/types.h>
+#include <sys/time.h>
 #define MAX_BUFFER_SIZE 10240
 
+void receive_with_timeout(BIO *bio, char *total_buffer, char *recv_buffer, size_t buffer_size) {
+    fd_set read_fds;
+    FD_ZERO(&read_fds);
+    FD_SET(BIO_get_fd(bio, NULL), &read_fds);
 
+    struct timeval timeout;
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 0;
+    
+    while (select(BIO_get_fd(bio, NULL) + 1, &read_fds, NULL, NULL, &timeout) > 0) {
+        BIO_read(bio, recv_buffer, 10240);
+        strcat(total_buffer, recv_buffer);
+    }
+}
+
+// Helper function to send and receive 
 void send_receive_ssl(BIO *bio, const char *send_cmd, char *recv_buffer, size_t buffer_size) {
     // Send command
     BIO_puts(bio, send_cmd);
@@ -32,6 +48,7 @@ int main(int argc, char *argv[]) {
     // Create SSL context and BIO
     SSL_CTX *ssl_ctx = SSL_CTX_new(SSLv23_client_method());
     BIO *bio = BIO_new_ssl_connect(ssl_ctx);
+    SSL_CTX_set_mode(ssl_ctx, SSL_MODE_AUTO_RETRY);
 
     // Set up connection
     BIO_set_conn_hostname(bio, hostname);
@@ -53,51 +70,66 @@ int main(int argc, char *argv[]) {
     char login_command[MAX_BUFFER_SIZE];
     sprintf(login_command, "A01 LOGIN %s {%lu}\r\n%s\r\n", username, strlen(password), password);
     char login_response[MAX_BUFFER_SIZE];
-    send_receive_ssl(bio, login_command, login_response, sizeof(login_response));
-    printf("Login Response: %s\n", login_response);
+    char total_login[102400];
+    BIO_puts(bio, login_command);
+
+    // Use select to check if data is available to be read
+
+    receive_with_timeout(bio, total_login, login_response, 10240);
+    
+    //send_receive_ssl(bio, login_command, login_response, sizeof(login_response));
+    
+
+
+    
+
+    printf("login response:\n%s\n", total_login);
+
 
     
 
     // Select the INBOX folder
     char select_command[] = "A02 SELECT INBOX\r\n";
     char select_response[MAX_BUFFER_SIZE];
-    send_receive_ssl(bio, select_command, select_response, sizeof(select_response));
+    char total_select[102400];
+    BIO_puts(bio, select_command);
+    receive_with_timeout(bio, total_select, select_response, 10240);
+
     printf("Select Response: %s\n", select_response);
 
     // Fetch the first email
     char fetch_command[] = "A03 FETCH 1 BODY.PEEK[]\r\n";
     char fetch_response[MAX_BUFFER_SIZE];
-    char combined[102400];
+    char total_fetch[102400];
+    BIO_puts(bio, fetch_command);
+    receive_with_timeout(bio, total_fetch, fetch_response, 10240);
+    printf("Email body: %s\n", total_fetch);
     
-    // BIO_read does not always read the full response so I have to call it multiple times. Also, since SSL BIO is blocking, 
-    // if there is no new data to be read, BIO_read will not actually return and will just keep waiting for a response. 
-    // Therefore, I will just keep sending fetch commands so there will always be something to read.
-    // The amount of times that I need to call send_receive_ssl is arbitrary but it just needs to be long enough to not miss out on any details.
-    for (int i = 0; i < 20; i++) {
-        //BIO_read(bio, fetch_response, 10240);
-        send_receive_ssl(bio, fetch_command, fetch_response, 10240);
-        strcat(combined, fetch_response);
-    }
-    //printf("combined:\n%s\n", combined);
+    // // BIO_read does not always read the full response so I have to call it multiple times. Also, since SSL BIO is blocking, 
+    // // if there is no new data to be read, BIO_read will not actually return and will just keep waiting for a response. 
+    // // Therefore, I will just keep sending fetch commands so there will always be something to read.
+    // // The amount of times that I need to call send_receive_ssl is arbitrary but it just needs to be long enough to not miss out on any details.
+    // for (int i = 0; i < 20; i++) {
+    //     //BIO_read(bio, fetch_response, 10240);
+    //     send_receive_ssl(bio, fetch_command, fetch_response, 10240);
+    //     strcat(combined, fetch_response);
+    // }
+    // //printf("combined:\n%s\n", combined);
 
-    // This code is for manipulating the strings to extract the email body, starting at "From:"
-    // I noticed that the email contents began and ended at the parts with double dashes (--) followed by random numbers and letters
-    char* from_pos;
-    from_pos = strstr(combined, "From");
-    char* first_dash = strstr(from_pos, "--");
-    char* second_dash = strstr(first_dash + 2, "--");
-    size_t body_length = second_dash - from_pos - 2;
-    char* email_body = malloc(body_length + 1);
-    strncpy(email_body, from_pos, body_length);
-    email_body[body_length] = '\0';
-
-    printf("Email Body:\n%s\n", email_body);
-
-    // send_receive_ssl(bio, fetch_command, fetch_response, sizeof(fetch_response));
+    // // This code is for manipulating the strings to extract the email body, starting at "From:"
+    // // I noticed that the email contents began and ended at the parts with double dashes (--) followed by random numbers and letters
+    // char* from_pos;
+    // from_pos = strstr(combined, "From");
+    // char* first_dash = strstr(from_pos, "--");
+    // char* second_dash = strstr(first_dash + 2, "--");
+    // size_t body_length = second_dash - from_pos - 2;
+    // char* email_body = malloc(body_length + 1);
+    // strncpy(email_body, from_pos, body_length);
+    // email_body[body_length] = '\0';
 
     // // Display the email body
-    // printf("Email Body:\n%s\n", fetch_response);
-    
+    // printf("Email Body:\n%s\n", email_body);
+
     
     
 
