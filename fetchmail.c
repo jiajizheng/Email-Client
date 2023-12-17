0d6735c608cae0c7fcddbd87b6361ff8b72ae220
Jia Jie Zheng <jiajizheng@student.unimelb.edu.au>
2023-12-17 21:07:59 +1100

    code works for longer emails now

--- fetchmail.c
+++ fetchmail.c
@@ -3,10 +3,16 @@
 #include <string.h>
 #include <openssl/ssl.h>
 #include <openssl/bio.h>
+#include <openssl/conf.h>
 #include <sys/types.h>
 #include <sys/time.h>
 #define MAX_BUFFER_SIZE 10240
 
+
+// BIO_read does not always read the full response so I have to call it multiple times. Also, since SSL BIO is blocking, 
+// if there is no new data to be read, BIO_read will not actually return and will just keep waiting for a response. 
+// Therefore, I used the select() system call with a timeout to prevent the program blocking indefinitely.
+// I took inspiration from the example code at https://www.gnu.org/software/libc/manual/html_node/Waiting-for-I_002fO.html
 void receive_with_timeout(BIO *bio, char *total_buffer, char *recv_buffer, size_t buffer_size) {
     fd_set read_fds;
     FD_ZERO(&read_fds);
@@ -17,23 +23,24 @@ void receive_with_timeout(BIO *bio, char *total_buffer, char *recv_buffer, size_
     timeout.tv_usec = 0;
     
     while (select(BIO_get_fd(bio, NULL) + 1, &read_fds, NULL, NULL, &timeout) > 0) {
-        BIO_read(bio, recv_buffer, 10240);
+        BIO_read(bio, recv_buffer, MAX_BUFFER_SIZE);
         strcat(total_buffer, recv_buffer);
     }
 }
 
-// Helper function to send and receive 
-void send_receive_ssl(BIO *bio, const char *send_cmd, char *recv_buffer, size_t buffer_size) {
-    // Send command
-    BIO_puts(bio, send_cmd);
+// I wrote this helper function when I assumed that BIO_read will read the entire response.
+// // Helper function to send and receive 
+// void send_receive_ssl(BIO *bio, const char *send_cmd, char *recv_buffer, size_t buffer_size) {
+//     // Send command
+//     BIO_puts(bio, send_cmd);
 
-    // Receive response
-    BIO_read(bio, recv_buffer, buffer_size);
-}
+//     // Receive response
+//     BIO_read(bio, recv_buffer, buffer_size);
+// }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <hostname>\n", argv[0]);
+        perror("Usage: ./fetchmail <hostname>");
         return EXIT_FAILURE;
     }
 
@@ -42,12 +49,23 @@ int main(int argc, char *argv[]) {
     const char *username = "tzeng0088@gmail.com";
     const char *password = "vhkycyvvnoxcoejj";
 
+    // I took inspiration from https://wiki.openssl.org/index.php/SSL/TLS_Client when setting up the SSL BIO connection.
     SSL_library_init();
     SSL_load_error_strings();
 
     // Create SSL context and BIO
     SSL_CTX *ssl_ctx = SSL_CTX_new(SSLv23_client_method());
+    if(!(NULL != ssl_ctx)) {
+        perror("Failure initializing SLL_CTX");
+        return EXIT_FAILURE;
+    }
+
     BIO *bio = BIO_new_ssl_connect(ssl_ctx);
+    if(!(NULL != bio)) {
+        perror("Failure initializing BIO");
+        return EXIT_FAILURE;
+    }
+    
     SSL_CTX_set_mode(ssl_ctx, SSL_MODE_AUTO_RETRY);
 
     // Set up connection
@@ -55,14 +73,14 @@ int main(int argc, char *argv[]) {
     BIO_set_conn_port(bio, "imaps");
 
     // Open connection
-    if (BIO_do_connect(bio) <= 0) {
-        fprintf(stderr, "Error: Failed to connect to the server.\n");
+    if (!(1 == BIO_do_connect(bio)))  {
+        perror("Failed to connect to the server");
         return EXIT_FAILURE;
     }
 
     // Perform TLS handshake
-    if (BIO_do_handshake(bio) <= 0) {
-        fprintf(stderr, "Error: Failed TLS handshake.\n");
+    if (!(1 == BIO_do_handshake(bio))) {
+        perror("Failed to perform TLS handshake");
         return EXIT_FAILURE;
     }
 
@@ -72,43 +90,27 @@ int main(int argc, char *argv[]) {
     char login_response[MAX_BUFFER_SIZE];
     char total_login[102400];
     BIO_puts(bio, login_command);
-
-    // Use select to check if data is available to be read
-
-    receive_with_timeout(bio, total_login, login_response, 10240);
-    
-    //send_receive_ssl(bio, login_command, login_response, sizeof(login_response));
-    
-
-
-    
-
+    receive_with_timeout(bio, total_login, login_response, MAX_BUFFER_SIZE);
     printf("login response:\n%s\n", total_login);
-
-
-    
 
     // Select the INBOX folder
     char select_command[] = "A02 SELECT INBOX\r\n";
     char select_response[MAX_BUFFER_SIZE];
     char total_select[102400];
     BIO_puts(bio, select_command);
-    receive_with_timeout(bio, total_select, select_response, 10240);
-
-    printf("Select Response: %s\n", select_response);
+    receive_with_timeout(bio, total_select, select_response, MAX_BUFFER_SIZE);
+    printf("Select Response: \n%s\n", select_response);
 
     // Fetch the first email
     char fetch_command[] = "A03 FETCH 1 BODY.PEEK[]\r\n";
     char fetch_response[MAX_BUFFER_SIZE];
     char total_fetch[102400];
     BIO_puts(bio, fetch_command);
-    receive_with_timeout(bio, total_fetch, fetch_response, 10240);
-    printf("Email body: %s\n", total_fetch);
+    receive_with_timeout(bio, total_fetch, fetch_response, MAX_BUFFER_SIZE);
+    printf("Email body: \n%s\n", total_fetch);
     
-    // // BIO_read does not always read the full response so I have to call it multiple times. Also, since SSL BIO is blocking, 
-    // // if there is no new data to be read, BIO_read will not actually return and will just keep waiting for a response. 
-    // // Therefore, I will just keep sending fetch commands so there will always be something to read.
-    // // The amount of times that I need to call send_receive_ssl is arbitrary but it just needs to be long enough to not miss out on any details.
+    
+    // Old code, hard-coded stuff
     // for (int i = 0; i < 20; i++) {
     //     //BIO_read(bio, fetch_response, 10240);
     //     send_receive_ssl(bio, fetch_command, fetch_response, 10240);
@@ -126,12 +128,6 @@ int main(int argc, char *argv[]) {
     // char* email_body = malloc(body_length + 1);
     // strncpy(email_body, from_pos, body_length);
     // email_body[body_length] = '\0';
-
-    // // Display the email body
-    // printf("Email Body:\n%s\n", email_body);
-
-    
-    
 
     // Clean up
     BIO_free_all(bio);
