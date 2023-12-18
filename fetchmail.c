26b4d6ce0c8019698a79444f60aca1f88437b77f
Jia Jie Zheng <jiajizheng@student.unimelb.edu.au>
2023-12-18 14:50:03 +1100

    Cleaning up code and comments

--- fetchmail.c
+++ fetchmail.c
@@ -8,7 +8,7 @@
 #include <sys/time.h>
 #define MAX_BUFFER_SIZE 10240
 
-
+// This is a function that calls BIO_read in a loop until there is no data to be read.
 // BIO_read does not always read the full response so I have to call it multiple times. Also, since SSL BIO is blocking, 
 // if there is no new data to be read, BIO_read will not actually return and will just keep waiting for a response. 
 // Therefore, I used the select() system call with a timeout to prevent the program blocking indefinitely.
@@ -19,7 +19,7 @@ void receive_with_timeout(BIO *bio, char *total_buffer, char *recv_buffer, size_
     FD_SET(BIO_get_fd(bio, NULL), &read_fds);
 
     struct timeval timeout;
-    timeout.tv_sec = 5;
+    timeout.tv_sec = 3;
     timeout.tv_usec = 0;
     
     while (select(BIO_get_fd(bio, NULL) + 1, &read_fds, NULL, NULL, &timeout) > 0) {
@@ -27,16 +27,6 @@ void receive_with_timeout(BIO *bio, char *total_buffer, char *recv_buffer, size_
         strcat(total_buffer, recv_buffer);
     }
 }
-
-// I wrote this helper function when I assumed that BIO_read will read the entire response.
-// // Helper function to send and receive 
-// void send_receive_ssl(BIO *bio, const char *send_cmd, char *recv_buffer, size_t buffer_size) {
-//     // Send command
-//     BIO_puts(bio, send_cmd);
-
-//     // Receive response
-//     BIO_read(bio, recv_buffer, buffer_size);
-// }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -84,11 +74,11 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    // Log in to the IMAP server
+    // Log in to the Gmail IMAP server
     char login_command[MAX_BUFFER_SIZE];
     sprintf(login_command, "A01 LOGIN %s {%lu}\r\n%s\r\n", username, strlen(password), password);
     char login_response[MAX_BUFFER_SIZE];
-    char total_login[102400];
+    char total_login[MAX_BUFFER_SIZE];
     BIO_puts(bio, login_command);
     receive_with_timeout(bio, total_login, login_response, MAX_BUFFER_SIZE);
     printf("login response:\n%s\n", total_login);
@@ -96,7 +86,7 @@ int main(int argc, char *argv[]) {
     // Select the INBOX folder
     char select_command[] = "A02 SELECT INBOX\r\n";
     char select_response[MAX_BUFFER_SIZE];
-    char total_select[102400];
+    char total_select[MAX_BUFFER_SIZE];
     BIO_puts(bio, select_command);
     receive_with_timeout(bio, total_select, select_response, MAX_BUFFER_SIZE);
     printf("Select Response: \n%s\n", select_response);
@@ -104,24 +94,16 @@ int main(int argc, char *argv[]) {
     // Fetch the first email
     char fetch_command[] = "A03 FETCH 1 BODY.PEEK[]\r\n";
     char fetch_response[MAX_BUFFER_SIZE];
-    char total_fetch[102400];
+    char total_fetch[102400];  // Using a larger array because a very long email would crash the program
     BIO_puts(bio, fetch_command);
     receive_with_timeout(bio, total_fetch, fetch_response, MAX_BUFFER_SIZE);
     printf("Email body: \n%s\n", total_fetch);
-    
-    
-    // Old code, hard-coded stuff
-    // for (int i = 0; i < 20; i++) {
-    //     //BIO_read(bio, fetch_response, 10240);
-    //     send_receive_ssl(bio, fetch_command, fetch_response, 10240);
-    //     strcat(combined, fetch_response);
-    // }
-    // //printf("combined:\n%s\n", combined);
 
-    // // This code is for manipulating the strings to extract the email body, starting at "From:"
-    // // I noticed that the email contents began and ended at the parts with double dashes (--) followed by random numbers and letters
+    // Not sure if the following code is needed, 
+    // it is for manipulating the strings to extract what looks like an email, starting at "From:"
+    // I noticed that the email contents began and ended at the parts with double dashes (--) followed by random numbers and letters
     // char* from_pos;
-    // from_pos = strstr(combined, "From");
+    // from_pos = strstr(total_fetch, "From");
     // char* first_dash = strstr(from_pos, "--");
     // char* second_dash = strstr(first_dash + 2, "--");
     // size_t body_length = second_dash - from_pos - 2;
